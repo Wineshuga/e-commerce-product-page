1f550594c1ef2328988c9e2c03f3651dce92c9da
wineshuga <uziwinnie@gmail.com>
2022-11-29 14:55:39 +0100

    feat: lightbox update

--- index.js
+++ index.js
@@ -22,3 +22,48 @@ function noMenuBar() {
 
 document.getElementById('header--menu_icon').addEventListener('click', menuBar)
 document.getElementById('close').addEventListener('click', noMenuBar);
+
+// TO DISPLAY MODAL ON DESKTOP
+
+function openModal() {
+    document.getElementById('myModal').style.display = 'flex';
+}
+
+// TO HIDE MODAL ON DESKTOP
+
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+}
+
+// TO SLIDE IMAGES ON DESKTOP AND MOBILE
+
+let slideIndex = 1;
+showSlides(slideIndex);
+// previous and next arrows
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+// current images
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("main--slide");
+    let dots = document.getElementsByClassName("demo");
+    let i;
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "")
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
