7f6dc888f0db5922194b06d9e14cfa427236705a
wineshuga <uziwinnie@gmail.com>
2022-12-01 22:39:22 +0100

    feat: include increment and decrement

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const menu = document.querySelector('#mobile--menu');
 
 //Hamburger menu show on mobile view
-
 function menuBar() {
     if (menu.style.display == 'none') {
         menu.style.display = 'block'
@@ -11,7 +10,6 @@ function menuBar() {
 }
 
 //Hamburger menu show on mobile view
-
 function noMenuBar() {
     if (menu.style.display == 'block') {
         menu.style.display = 'none'
@@ -24,19 +22,16 @@ document.getElementById('header--menu_icon').addEventListener('click', menuBar)
 document.getElementById('close').addEventListener('click', noMenuBar);
 
 // TO DISPLAY MODAL ON DESKTOP
-
 function openModal() {
     document.getElementById('myModal').style.display = 'flex';
 }
 
 // TO HIDE MODAL ON DESKTOP
-
 function closeModal() {
     document.getElementById('myModal').style.display = 'none';
 }
 
 // TO SLIDE IMAGES ON DESKTOP AND MOBILE
-
 let slideIndex = 1;
 showSlides(slideIndex);
 // previous and next arrows
@@ -67,3 +62,33 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+// INCREMENT AND DECREMENT
+
+// select the counter
+let totalCount = document.getElementById("item--amount")
+// initialize the amount
+let count = 0
+// display the count
+totalCount.innerHTML = count;
+// Function to increment count
+const handleIncrement = () => {
+    count++;
+    totalCount.innerHTML = count;
+};
+
+// Function to decrement count
+const handleDecrement = () => {
+    if (count > 0) {
+        count--;
+    }
+    totalCount.innerHTML = count;
+};
+
+// Select increment and decrement buttons
+const incrementCount = document.getElementById("plus");
+const decrementCount = document.getElementById("minus");
+
+// Add click event to buttons
+plus.addEventListener("click", handleIncrement);
+minus.addEventListener("click", handleDecrement);
